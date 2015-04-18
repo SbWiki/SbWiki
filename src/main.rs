01c33628b6038b46b499a915373e5a19d450ccf0
perillamint <perillamint@gmail.com>
2015-04-19 02:40:39 +0900

    Iron and Router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate iron;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::{Router};
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = Router::new();
+
+    router.get("/", roothandler);
+    router.get("/:query", roothandler);
+
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn roothandler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>()
+            .unwrap().find("query").unwrap_or("/");
+
+        Ok(Response::with((status::Ok, *query)))
+    }
 }
