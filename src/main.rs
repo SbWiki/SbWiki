0b8c66b4a63bef529c2cbf6b42bf8fbb1334bb3a
perillamint <perillamint@gmail.com>
2015-04-19 22:31:16 +0900

    Redirect handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+#![feature(collections)]
+
+extern crate toml;
 extern crate iron;
 extern crate router;
 
 use iron::prelude::*;
-use iron::status;
+use iron::headers::*;
+use iron::{status, headers};
+
 use router::{Router};
+
 
 fn main() {
     let mut router = Router::new();
 
+    //TODO: fetch it from toml config.
+    router.get("/wiki", wikiredirect);
+    router.get("/wiki/*", wikihandler);
+    
     router.get("/", roothandler);
     router.get("/:query", roothandler);
 
@@ -18,5 +28,38 @@ fn main() {
             .unwrap().find("query").unwrap_or("/");
 
         Ok(Response::with((status::Ok, *query)))
+    }
+
+    fn wikiredirect(req: &mut Request) -> IronResult<Response> {
+        let mut url = req.url.path.clone();
+        let mut header = Headers::new();
+        let mut newpath = String::new();
+
+        //TODO: customizable root page.
+        url.push(String::from_str("FrontPage"));
+
+        for i in url.iter() {
+            newpath.push_str("/");
+            newpath.push_str(i);
+        }
+
+        println!("{}", newpath);
+        header.set(Location(newpath));
+
+        let mut resp: Response = Response::with((status::MovedPermanently));
+        resp.headers = header;
+        
+        Ok(resp)
+    }
+
+    fn wikihandler(req: &mut Request) -> IronResult<Response> {
+        let ref requrl = req.url.clone();
+        let mut urlpath = requrl.path.clone();
+        
+        for i in urlpath.iter() {
+            println!("{}\n", i);
+        }
+
+        Ok(Response::with((status::Ok, "Watch console.")))
     }
 }
