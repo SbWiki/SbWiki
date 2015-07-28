1e2da6aad92b2e1963db89bcb8b7a035dba34773
perillamint <perillamint@gentoo.moe>
2015-07-28 21:10:54 +0900

    Remove lifetime specifiers. Still not working

--- src/liquidwrapper.rs
+++ src/liquidwrapper.rs
@@ -3,6 +3,7 @@ extern crate liquid;
 use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
+use std::default::Default;
 
 use liquid::Context;
 use liquid::Renderable;
@@ -17,17 +18,19 @@ pub struct LiquidTemplate {
     liquid_template: Box<Renderable>,
 }
 
-impl<'a> TemplateWrapper for LiquidTemplate {
+impl TemplateWrapper for LiquidTemplate {
     fn new(template_string: String) -> LiquidTemplate {
-        let mut options = Default::default();
-        let template = parse(&template_string, &mut options).unwrap();
-        
+        let mut options : LiquidOptions = Default::default();
+
+        let template : Box<Renderable> =
+            Box::new(parse(&template_string, options).unwrap());
+
         LiquidTemplate {
             template_string: template_string,
-            liquid_template: Box::new(template),
+            liquid_template: template,
         }
     }
-    
+
     fn render(&self, data: HashMap<String, String>) -> String {
         let mut con = Context::new();
         
